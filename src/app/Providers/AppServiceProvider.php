e8cc3248149b005d7d87e02b73cdfe0d738aa2e0
AgentFUD <agentfud@gmail.com>
2023-10-10 16:14:34 +0200

    added missing import

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Event;
+use App\Observers\RecurrenceOserver;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Carbon;
